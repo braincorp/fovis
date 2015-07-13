f3a66fd592775bfbeae14b03bc239c938c67a677
Andrew T. Smith <smith@braincorporation.com>
2015-07-13 11:59:16 -0700

    made symmetric

--- fovis_ros/src/odometer_base.hpp
+++ fovis_ros/src/odometer_base.hpp
@@ -188,8 +188,10 @@ protected:
             odom_msg_.twist.covariance[j*6+i] = motion_cov(i,j);
 	srand(47);  // get same covariance values
         for (int i=0;i<6;i++)
-          for (int j=0;j<6;j++){
-            odom_msg_.pose.covariance[j*6+i] = i==j ? 1.0e-7 : random_sign()*1.0e-11;
+          for (int j=0;j<=i;j++){
+	    double cov_val =  i==j ? 1.0e-7 : random_sign()*1.0e-11;
+            odom_msg_.pose.covariance[j*6+i] = cov_val;
+	    odom_msg_.pose.covariance[i*6+j] = cov_val;
           }
 
       }
